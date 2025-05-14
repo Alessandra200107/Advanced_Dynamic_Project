28da50ba8584ae9feadab705195192ce14e6b04e
Alessandra200107 <alessandra1.vitali@mail.polimi.it>
2025-05-14 17:49:41 +0200

    speremmm

--- Assignment1_B.m
+++ Assignment1_B.m
@@ -35,13 +35,35 @@ for i=1:12
     hold on
     grid on
 end
+% mag=zeros(length(f),12);
+% omega=zeros(length(f),12);
+% for i=1:12
+%     [mag(:,i), omega(:,i)]=findpeaks(magnitude(:,i),f,'MinPeakProminence', 0.05);
+% end
+% i_nat = [];
+% 
+% for i=2:length(f)-1
+%     if magnitude(i,1) < magnitude(i-1,1) && magnitude(i,1) < magnitude(i+1,1)
+%         i_nat(end+1) = i;
+%     end
+% end
+% 
+% omega = f (i_nat);
+mag_c = cell(1,12);
+omega_c = cell(1,12);
 
-i_nat = [];
-
-for i=2:length(f)-1
-    if magnitude(i,1) < magnitude(i-1,1) && magnitude(i,1) < magnitude(i+1,1)
-        i_nat(end+1) = i;
-    end
+for i = 1:12
+    [mag_c{i}, omega_c{i}] = findpeaks(magnitude(:,i), f, 'MinPeakProminence', 0.1);
 end
 
-omega = f (i_nat);
+mag = nan(2, 12);    
+omega = nan(2, 12);
+
+for i = 1:12
+    omega(1:2, i) = omega_c{i}(1:2);
+    mag(1:2, i) = mag_c{i}(1:2);
+end
+
+
+
+
