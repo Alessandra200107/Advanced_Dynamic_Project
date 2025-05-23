7b71365b61482659289172b3c1350f884c58afa7
annapaglino <anna.paglino@mail.polimi.it>
2025-05-23 16:30:24 +0200

    cosa

--- Assignment_2_FEM.m
+++ Assignment_2_FEM.m
@@ -1,2 +1,11 @@
 %% ASSIGNMENT 2 - FEM
 
+clear all
+close all
+clc
+
+% load input file and assemble structure
+[file_i,xy,nnod,sizee,idb,ndof,incidenze,l,gamma,m,EA,EJ,posiz,nbeam,pr]=loadstructure;
+
+% draw structure
+dis_stru(posiz,l,gamma,xy,pr,idb,ndof);
